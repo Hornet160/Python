4a337dd42bab092de1f287e09f77aab41f62e1c2
Mosharaf <91388010+Hornet160@users.noreply.github.com>
2023-05-25 13:40:57 +0600

    Update 12. Rock paper Scissor.py

--- 12. Rock paper Scissor.py
+++ 12. Rock paper Scissor.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -25,26 +26,26 @@ scissors = '''
 ---.__(___)
 '''
 
-#  code is below this line 👇
-import random
+# code is below this line 👇
+
 
 games_image = [rock, paper, scissors]
 computer_choice = random.randint(0, 2)
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors.\n"))
-print(games_image[user_choice])
-print("Computer Chose:")
-print(computer_choice)
-print(games_image[computer_choice])
-
-if user_choice >= 3 or user_choice < 0:
-    print("You entered invalid number")
-elif user_choice == 0 and computer_choice == 2:
-    print("You Win")
-elif user_choice == 2 and computer_choice == 0:
-    print("You lose")
-elif user_choice > computer_choice:
-    print("You Win")
-elif user_choice < computer_choice:
-    print("You lose")
-elif user_choice == computer_choice:
-    print("It is a draw")
+if user_choice>=3 or user_choice<0:
+    print("You Entered invalid number")
+else:
+    print(games_image[user_choice])
+    print("Computer Chose:")
+    print(computer_choice)
+    print(games_image[computer_choice])
+    if user_choice == 0 and computer_choice == 2:
+        print("You Win")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You Win")
+    elif user_choice < computer_choice:
+        print("You lose")
+    elif user_choice == computer_choice:
+        print("It is a draw")
